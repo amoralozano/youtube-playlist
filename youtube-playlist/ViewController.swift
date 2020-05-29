a3625613411fa065fd4889da04269f9236980101
amoralozano <66109164+amoralozano@users.noreply.github.com>
2020-05-29 00:13:34 -0700

    initial xcode project creation

---
+++ youtube-playlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-playlist
+//
+//  Created by Abdael Mora on 5/28/20.
+//  Copyright © 2020 Abdael Mora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
